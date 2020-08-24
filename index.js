3a8eb2c2c4bd12119e33725c199993131a0c1522
Ibrahim Riyami <iariyami@ncsu.edu>
2020-08-24 18:53:52 -0400

    added questions

--- index.js
+++ index.js
@@ -3,10 +3,52 @@ const fs = require("fs");
 const util = require("util");
 
 // array of questions for user
-const questions = [
-
-];
-
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project name?",
+        },
+        {
+            type: "editor",
+            name: "description",
+            message: "Provide a description of your project.",
+        },
+        {
+            type: "list",
+            name: "license",
+            message:
+                "Choose a license.",
+            choices: ["MIT", "Apache", "ISC", "None"],
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Who are the contributors?",
+        },
+        {
+            type: "editor",
+            name: "installation",
+            message: "How do you install the application?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email address",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What are the instructions for using this application?",
+        },
+    ]);
+}
 // function to write README file
 function writeToFile(fileName, data) {
 }
